cd8161d515938bb02e084522a8465d12f88d82c7
Olushi Luqman O <olushilukmon03@gmail.com>
2018-09-24 03:25:14 +0100

    Register middlewares

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import registerMiddlewares from './middlewares';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+registerMiddlewares(app);
 
 app.listen(PORT, () => {
   console.log('Server listening on port ');
