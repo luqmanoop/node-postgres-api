cc446353e0132d65a573646c62879212c22c2894
Olushi Luqman O <olushilukmon03@gmail.com>
2018-09-24 22:28:39 +0100

    Register model
    Create a DB transaction for running multiple model create operations as a single unit of work

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,25 @@
 import { Pool } from 'pg';
 import env from 'dotenv';
+import models from './models';
 
 env.config();
 const pool = new Pool({
   connectionString: process.env.DB_URL,
 });
+
+(async () => {
+  const client = await pool.connect();
+  try {
+    // start transaction
+    await client.query('BEGIN');
+    await models(client);
+    await client.query('COMMIT');
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(error);
+  } finally {
+    client.release();
+  }
+})();
 
 export default pool;
