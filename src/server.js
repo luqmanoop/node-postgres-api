80b1a1244eb497cad3594528dc37f435fec8bb69
Olushi Luqman O <olushilukmon03@gmail.com>
2018-09-24 04:26:50 +0100

    Mount API router

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import registerMiddlewares from './middlewares';
+import apiRouter from './router';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 registerMiddlewares(app);
+app.use('/api/v1', apiRouter);
 
 app.listen(PORT, () => {
-  console.log('Server listening on port ');
+  console.log(`Server listening on port ${PORT}`);
 });
