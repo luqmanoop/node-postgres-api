d80c21d48d81845ce2e66f2059204156d996efd5
Olushi Luqman O <olushilukmon03@gmail.com>
2018-09-25 09:20:27 +0100

    Change column data type

--- src/db/models/author.js
+++ src/db/models/author.js
@@ -1,6 +1,6 @@
 export default {
   CREATE_TABLE: `CREATE TABLE IF NOT EXISTS authors (
-        id UUID PRIMARY KEY,
+        id serial PRIMARY KEY,
         name VARCHAR (150) NOT NULL
     )`,
 };
--- src/db/models/post.js
+++ src/db/models/post.js
@@ -1,7 +1,7 @@
 export default {
   CREATE_TABLE: `CREATE TABLE IF NOT EXISTS posts (
-    id UUID PRIMARY KEY,
-    author_id UUID REFERENCES authors(id) ON DELETE CASCADE,
+    id serial PRIMARY KEY,
+    author_id serial REFERENCES authors(id) ON DELETE CASCADE,
     title VARCHAR (150) NOT NULL,
     slug VARCHAR(150),
     content text,
