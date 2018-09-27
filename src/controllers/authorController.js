a5c9e013dd134575fc248b0e446c2fda0a8c3b14
Olushi Luqman O <olushilukmon03@gmail.com>
2018-09-27 07:36:45 +0100

    Add method to create author in DB

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -2,11 +2,26 @@ import db from '../db';
 
 export default class AuthorController {
   /**
-   * Get authors from database
+   * Get authors from database.
    */
   static get(req, res) {
     db.query('SELECT id, name FROM authors')
       .then(result => res.send(result.rows))
-      .catch(err => res.status(500).send(err.message));
+      .catch(err => res.status(500).send({ message: err.message }));
+  }
+
+  /**
+   * Creates an author in DB.
+   */
+  static create(req, res) {
+    const { name } = req.body || {};
+    if (!name) {
+      res.status(400).send({ message: 'Author name is required.' });
+      return;
+    }
+
+    db.query('INSERT INTO authors (name) VALUES ($1)', [name])
+      .then(() => res.status(201).send())
+      .catch(err => res.status(500).send({ message: err.message }));
   }
 }
