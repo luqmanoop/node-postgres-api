db6eb6d916e9f35334d8c537af270cf4fee48e99
Olushi Luqman O <olushilukmon03@gmail.com>
2018-09-27 08:41:26 +0100

    Add middleware for finding post by ID

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,3 +1,23 @@
 import db from '../db';
 
-export default class PostController {}
+const TABLE = 'posts';
+export default class PostController {
+  static findById(req, res, next) {
+    const { id } = req.params || {};
+
+    if (!id) {
+      res.status(400).send({ message: 'Please provide post ID.' });
+      return;
+    }
+
+    db.query(`SELECT COUNT(*) FROM ${TABLE} WHERE id = $1`, [id])
+      .then(({ rows }) => {
+        if (rows[0].count <= 0) return res.status(404).send({ message: `Post with ID "${id}" not found.` });
+
+        // put the author id on request object
+        req.postId = id;
+        return next();
+      })
+      .catch(({ message }) => res.status(500).send({ message }));
+  }
+}
