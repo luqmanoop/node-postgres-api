827ed9a8bbb4109ca67fe2803cfe8e4ddd551b3a
Olushi Luqman O <olushilukmon03@gmail.com>
2018-09-27 08:02:16 +0100

    Add middleware for finding author ID

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,6 +1,25 @@
 import db from '../db';
 
 export default class AuthorController {
+  static findById(req, res, next) {
+    const { id } = req.params || {};
+
+    if (!id) {
+      res.status(400).send({ message: 'Please provide author ID.' });
+      return;
+    }
+
+    db.query('SELECT COUNT(*) FROM authors WHERE id = $1', [id])
+      .then(({ rows }) => {
+        if (rows[0].count <= 0) return res.status(404).send({ message: `Author with ID "${id}" not found.` });
+
+        // put the author id on request object
+        req.authorId = id;
+        return next();
+      })
+      .catch(({ message }) => res.status(500).send({ message }));
+  }
+
   /**
    * Get authors from database.
    */
