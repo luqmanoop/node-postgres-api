3ddd8d33aadea7585d708bc465e1c0aafa9f895d
Olushi Luqman O <olushilukmon03@gmail.com>
2018-09-27 07:23:41 +0100

    Add method for getting authors from database

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,3 +1,12 @@
 import db from '../db';
 
-export default class AuthorController {}
+export default class AuthorController {
+  /**
+   * Get authors from database
+   */
+  static get(req, res) {
+    db.query('SELECT id, name FROM authors')
+      .then(result => res.send(result.rows))
+      .catch(err => res.status(500).send(err.message));
+  }
+}
