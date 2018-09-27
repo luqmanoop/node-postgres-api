eafc2d6b1741b8fa9c6a72939f10c76cd906df02
Olushi Luqman O <olushilukmon03@gmail.com>
2018-09-27 08:49:24 +0100

    Register routers

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import registerMiddlewares from './middlewares';
-import apiRouter from './router';
+import registerRouter from './router';
 import './db';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 registerMiddlewares(app);
-app.use('/api/v1', apiRouter);
+registerRouter(app);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
