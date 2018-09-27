e77ae70a621742fd00a4eed478eaf5a37b35b638
Olushi Luqman O <olushilukmon03@gmail.com>
2018-09-27 08:50:45 +0100

    Mount post router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import authorRouter from './authorRouter';
+import postRouter from './postRouter';
 
 export default (app) => {
   app.get('/api/v1/', (req, res) => {
     res.json({ message: 'Awesome blog API yo!' });
   });
 
-  app.use('/api/v1/', [authorRouter]);
+  app.use('/api/v1/', [authorRouter, postRouter]);
 };
